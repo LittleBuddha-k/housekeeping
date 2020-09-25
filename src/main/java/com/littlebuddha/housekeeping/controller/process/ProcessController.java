00d465cff55fa367cee8c196b60bd90346826d46
LittleBuddha-k <2998986128@qq.com>
2020-09-25 16:32:27 +0800

    000

---
+++ src/main/java/com/littlebuddha/housekeeping/controller/process/ProcessController.java
@@ -0,0 +1,12 @@
+package com.littlebuddha.housekeeping.controller.process;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ck
+ * @date 2020/9/25 15:47
+ */
+@Controller
+public class ProcessController {
+
+}
